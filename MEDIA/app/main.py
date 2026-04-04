f0002c03ea1307c076674648876b30ef81775d2e
calmkeep22 <lapaella1200422@gmail.com>
2026-04-04 19:43:49 +0900

    [Feat] hls 스트리밍 구현

--- MEDIA/app/main.py
+++ MEDIA/app/main.py
@@ -1,26 +1,24 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
 from app.api.health import router as health_router
+from app.api.stream import router as stream_router
 from app.core.config import settings
-from app.core.logging import setup_logging
 from app.utils.file_utils import ensure_dirs
-
-
-setup_logging()
 
 app = FastAPI(title=settings.app_name)
 
-
-@app.on_event("startup")
-def on_startup() -> None:
-    ensure_dirs(
-        [
-            settings.incoming_dir,
-            settings.hls_dir,
-            settings.frames_dir,
-            settings.thumbnails_dir,
-            settings.clips_dir,
-        ]
-    )
-
+ensure_dirs(
+    [
+        settings.incoming_dir,
+        settings.hls_dir,
+        settings.frames_dir,
+        settings.thumbnails_dir,
+        settings.clips_dir,
+    ]
+)
 
 app.include_router(health_router)
+app.include_router(stream_router)
+
+app.mount("/hls", StaticFiles(directory=settings.hls_dir), name="hls")
