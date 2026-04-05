1c2491009687e99222fda734814ed1e1cfcf70b5
pkhyrn268 <bokddang@dgu.ac.kr>
2026-04-05 10:37:31 +0000

    remove: legacy YOLO-based pipeline

--- AI/yolo_tracking/src/tracking.py
+++
@@ -1,8 +0,0 @@
-import supervision as sv
-
-class Tracker:
-    def __init__(self):
-        self.tracker = sv.ByteTrack()
-
-    def update(self, detections):
-        return self.tracker.update_with_detections(detections)
