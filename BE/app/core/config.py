e48879e3eaca6d824bdca47e230fdd57901038d8
calmkeep22 <lapaella1200422@gmail.com>
2026-04-19 00:41:02 +0900

    [Fix] React 파일 수정

--- BE/app/core/config.py
+++ BE/app/core/config.py
@@ -29,6 +29,8 @@ class Settings(BaseSettings):
     jwt_algorithm: str = "HS256"
     jwt_expire_minutes: int = 60 * 24
 
+    cors_origins: list[str] = ["http://localhost:5173", "http://localhost:3000"]
+
     @property
     def database_url(self) -> str:
         return (
